945d038b5bbb1b7743ea00f4d1bf683fe7ef1eb9
Daler <dalermuhammadiev20@gmail.com>
2026-09-14 17:42:02 +0500

    Class work and Hom work

--- Person.cs
+++ Person.cs
@@ -1,4 +1,95 @@
-// Home Work namber 1 
+// class work namber 1 
+// 13.09.2026
+// Person person = new Person()
+// {
+//    FirstName = "Мухаммад",
+//    LastName = "Мирзоев" ,
+//   Age = 45,
+// };
+
+// Person person2 = new Person()
+// {
+//    FirstName = "Далер",
+//    LastName = "Мухаммадиев" ,
+//   Age = 16,
+// };
+//  person.GetInfo();
+// person2.GetInfo();
+
+
+// Post post1 = new Post();
+
+// post1.SetPost("Its C#");
+
+
+// for(int i =1 ; i<=10 ; i++)
+// {
+//     post1.AddLike();
+// }
+
+// post1.AddComment("Good");
+// post1.AddComment("good course for c#");
+
+// Console.WriteLine(post1.GetInfo());
+
+
+// List<string> comments =  post1.GetComments(); // comments =  { "Good" - 0 , "good course for c#" - 1 }
+
+// Console.WriteLine("Comments: ");
+
+// foreach (var item in comments) // item="Good" , item = "good course for c#"
+// {
+//     Console.WriteLine(item); // "Good" , "good course for c#"
+// }
+
+// for(int i=0 ; i<comments.Count ; i++)
+// {
+//     Console.WriteLine(comments[i]); // item = comments[i]
+// }
+
+
+// class work namber 3
+
+
+// Player player = new Player()
+// {
+//     Name = "Lionel Messi",
+//     Number = 10,
+//     Position = "Forward",
+//     Team = "Barcelona",
+// };
+// Console.WriteLine(player.GetInfo());
+// player.ScoreGoal();
+// player.AssistGoal("Neymar");
+
+
+// class work namber 4
+
+
+// Employee employee = new Employee()
+// {
+//     FirstName = "John",
+//     LastName = "Doe",
+//     Age = 30,
+//     Position = "Manager",
+//     Salary = 50000
+// };
+
+// Console.WriteLine("Output");
+// Console.WriteLine(employee.GetInfo());
+// Console.WriteLine(); 
+
+// employee.SetSalary(55000);
+
+// Console.WriteLine("Final Output");
+// Console.WriteLine($"Salary: {employee.GetSalary()}.0");
+
+
+
+
+
+
+// Home Work namber 1
 // 14.09.2026
 
 // Person person = new Person()
